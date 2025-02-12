5572714abe595dab7a34f07bef33e64578423da4
Mouhamed95 <lomoustapha95@gmail.com>
2025-02-12 12:12:53 +0000

    ecoute d'evenement

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,7 +10,16 @@ const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+  
 export class UserComponent {
  selectedUser = DUMMY_USERS[randomIndex]
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar 
+  }
+  
+  onSelected() {
+   console.log("selected")
+ }
 
 }
